f1e437fd8b0209fec441de5f6dda2d86d8b225c4
Melih Şimşek <Melihsimsek02@gmail.com>
2024-08-18 04:30:25 +0300

    Add project files.

---
+++ SuperMarketProject/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarketProject.Models;
+
+namespace SuperMarketProject.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetAllCategories();
+            return View(categories);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            Category? category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+
+            return View(category);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            CategoriesRepository.AddCategory(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? categoryId)
+        {
+            CategoriesRepository.DeleteCategory(categoryId.Value);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SuperMarketProject/Models/CategoriesRepository.cs
@@ -0,0 +1,62 @@
+﻿namespace SuperMarketProject.Models
+{
+    public static class CategoriesRepository
+    {
+        public static List<Category> _categories = new List<Category>()
+       {
+           new Category(){CategoryId=1,Name="Beverage",Description="This is a Beverage category"},
+           new Category(){CategoryId=2,Name="Bakery",Description="This is a Bakery category"},
+           new Category(){CategoryId=3,Name="Meat",Description="This is a Meat category"}
+       };
+
+
+        public static void AddCategory(Category category)
+        {
+            int maxId = _categories.Max(x => x.CategoryId);
+            category.CategoryId = (maxId + 1);
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetAllCategories()
+        {
+            return _categories;
+        }
+
+        public static Category GetCategoryById(int categoryId)
+        {
+            Category? category = _categories.FirstOrDefault(c => c.CategoryId == categoryId);
+            if(category != null)
+            {
+                return category;
+            }
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId,Category category)
+        {
+            Category? dbCategory = _categories.FirstOrDefault(c => c.CategoryId == categoryId);
+
+            if(category != null)
+            {
+                dbCategory.Description = category.Description;
+                dbCategory.Name = category.Name;
+                
+            }
+
+
+            return;
+        }
+
+
+        public static void DeleteCategory(int categoryId)
+        {
+            Category? category = GetCategoryById(categoryId);
+            if(category != null)
+            {
+                _categories.Remove(category);
+            }
+
+            return;
+        }
+    }
+}
